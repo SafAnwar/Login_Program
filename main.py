ef8c5ad175ba8294d04fd47bcc129dc3e9f5ed7f
SafAnwar <140514859+SafAnwar@users.noreply.github.com>
2025-01-16 19:55:14 +0000

    Day 6

---
+++ main.py
@@ -0,0 +1,14 @@
+#Day 6
+username = input("Enter the username: ")
+password = input("Enter the password: ")
+if username == "Saf":
+  if password == "17":
+    print("Welcome Saf!")
+  elif username == "Aanam":
+    if password == "19":
+      print("Welcome Aanam!")
+  elif username == "Fadi":
+    if password == "28":
+      print("Welcome Fadi!")
+else:
+  print("Wrong username or password")
